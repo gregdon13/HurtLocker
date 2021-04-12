0d3d95fba7f8963f0342c59812264c8f448edd3c
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-04-12 15:40:20 -0400

    getting even closer to a finished list of maps. Once I have that, I can just loop through, count and print

--- src/main/java/MapMaker.java
+++ src/main/java/MapMaker.java
@@ -12,35 +12,35 @@ public class MapMaker {
     }
 
 
-    public void mapMaker2() {
-        for (String s : listCreator.getGroceryArraylist()) {
-            Pattern namePattern = Pattern.compile("name[^A-Z0-9](.*?);", Pattern.CASE_INSENSITIVE);
-            Matcher matcher = namePattern.matcher(s);
-                while(matcher.find()) {
-                    for (int i = 0; i < listCreator.groceryMapList.size(); i++) {
-                        if (i == 0) {
-                            Grocery grocery = new Grocery();
-                            listCreator.groceryMapList.add(grocery.groceryMap);
-                            listCreator.groceryMapList.get(i).get("Name").add(matcher.group(1));
-                        }
-
-//                        else if (matcher.group().equals(listCreator.groceryMapList.get(i).get("Name").get(0))) {
+//    public void mapMaker2() {
+//        for (String s : listCreator.getGroceryArraylist()) {
+//            Pattern namePattern = Pattern.compile("name[^A-Z0-9](.*?);", Pattern.CASE_INSENSITIVE);
+//            Matcher matcher = namePattern.matcher(s);
+//                while(matcher.find()) {
+//                    for (int i = 0; i < listCreator.groceryMapList.size(); i++) {
+//                        if (i == 0) {
+//                            Grocery grocery = new Grocery();
+//                            listCreator.groceryMapList.add(grocery.groceryMap);
 //                            listCreator.groceryMapList.get(i).get("Name").add(matcher.group(1));
 //                        }
-                    }
-                }
-            Grocery grocery = new Grocery();
-            while(matcher.find()) {
-                grocery.groceryMap.get("Name").add(matcher.group(1));
-            }
-            Pattern pricePattern = Pattern.compile("price[^A-Z0-9](([0-9]*?)\\.[0-9][0-9])");
-            Matcher priceMatcher = pricePattern.matcher(s);
-            while(priceMatcher.find()){
-                grocery.groceryMap.get("Price").add(priceMatcher.group(1));
-            }
-            listCreator.groceryMapList.add(grocery.groceryMap);
-        }
-    }
+//
+////                        else if (matcher.group().equals(listCreator.groceryMapList.get(i).get("Name").get(0))) {
+////                            listCreator.groceryMapList.get(i).get("Name").add(matcher.group(1));
+////                        }
+//                    }
+//                }
+//            Grocery grocery = new Grocery();
+//            while(matcher.find()) {
+//                grocery.groceryMap.get("Name").add(matcher.group(1));
+//            }
+//            Pattern pricePattern = Pattern.compile("price[^A-Z0-9](([0-9]*?)\\.[0-9][0-9])");
+//            Matcher priceMatcher = pricePattern.matcher(s);
+//            while(priceMatcher.find()){
+//                grocery.groceryMap.get("Price").add(priceMatcher.group(1));
+//            }
+//            listCreator.groceryMapList.add(grocery.groceryMap);
+//        }
+//    }
 
     public void mapMaker() {
         for (int i = 0; i < listCreator.groceryArraylist.size(); i++) {
@@ -55,8 +55,22 @@ public class MapMaker {
                         grocery.groceryMap.get("Price").add(priceMatcher.group(1));
                         listCreator.groceryMapList.add(grocery.groceryMap);
                     } else {
+                        int listSize = listCreator.groceryMapList.size();
                         //catches if list of maps is NOT empty
-
+                        boolean addAGrocery = true;
+                        for (int j = 0; j < listCreator.groceryMapList.size(); j++) {
+                            if (matcher.group(1).equalsIgnoreCase(listCreator.groceryMapList.get(j).get("Name").get(0))) {
+                                listCreator.groceryMapList.get(j).get("Name").add(matcher.group(1));
+                                listCreator.groceryMapList.get(j).get("Price").add(priceMatcher.group(1));
+                                addAGrocery = false;
+                            }
+                        }
+                        if (addAGrocery) {
+                            Grocery grocery = new Grocery();
+                            grocery.groceryMap.get("Name").add(matcher.group(1));
+                            grocery.groceryMap.get("Price").add(priceMatcher.group(1));
+                            listCreator.groceryMapList.add(grocery.groceryMap);
+                        }
                     }
                 }
         }
