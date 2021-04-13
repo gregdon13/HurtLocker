233242bb61ad65bfafbd6b519db6879e70f073ba
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-04-12 22:17:25 -0400

    I THINK I GOT IT TO WORK FULLY FLEXIBLE

--- src/main/java/Grocery.java
+++ src/main/java/Grocery.java
@@ -4,15 +4,10 @@ import java.util.LinkedHashMap;
 public class Grocery {
     String name;
     String price;
-//    String type;
-//    String expiration;
     LinkedHashMap<String, ArrayList<String>> groceryMap = new LinkedHashMap<String, ArrayList<String>>();
-    ArrayList<String> numOfTimes;
 
     public Grocery(){
         groceryMap.put("Name", new ArrayList<String>());
         groceryMap.put("Price", new ArrayList<String>());
-//        groceryMap.put("Type", new ArrayList<String>());
-//        groceryMap.put("Expiration", new ArrayList<String>());
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public String readRawDataToString() throws Exception{
         ClassLoader classLoader = getClass().getClassLoader();
-        String result = IOUtils.toString(classLoader.getResourceAsStream("RawData.txt"));
+        String result = IOUtils.toString(classLoader.getResourceAsStream("justapples.txt"));
         return result;
     }
 
@@ -17,18 +17,11 @@ public class Main {
     public static void main(String[] args) throws Exception{
         String output = (new Main()).readRawDataToString();
         ListCreator listCreator = new ListCreator(output);
-        MapMaker mapMaker = new MapMaker();
         Counter counter = new Counter();
         listCreator.groceryMaker(output);
-        mapMaker.mapMaker();
         counter.countInstances();
-        //listCreator.makeNamesList();
-//        listCreator.distinctNames();
         System.out.println(listCreator.groceryArraylist);
-        //System.out.println(listCreator.namesList);
-        //System.out.println(mapMaker.listCreator.groceryMapList);
+        System.out.println(counter.mapMaker.listCreator.groceryMapList);
         System.out.println(counter.output);
-        //System.out.println(output);
-
     }
 }
